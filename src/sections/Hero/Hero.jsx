3ca8bbe96f008afc2ce60fb190a1a6230ef1f917
A.I.P.Jayasooriya <isurupromuditha617@gmail.com>
2024-12-11 00:18:28 +0530

    styling the hero section with theame switching

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
 import styles from './HeroStyles.module.css';
 import heroImg from '../../assets/hero-img.png';
-import themeIcon from '../../assets/sun.svg';
-import twitterIcon from '../../assets/twitter-light.svg';
-import linkedinIcon from '../../assets/linkedin-light.svg';
-import githubIcon from '../../assets/github-light.svg';
+import sun from '../../assets/sun.svg';
+import moon from '../../assets/moon.svg';
+import twitterLight from '../../assets/twitter-light.svg';
+import twitterDark from '../../assets/twitter-dark.svg';
+import linkedinLight from '../../assets/linkedin-light.svg';
+import linkedinDark from '../../assets/linkedin-dark.svg';
+import githubLight from '../../assets/github-light.svg';
+import githubDark from '../../assets/github-dark.svg';
 import CV from '../../assets/CV.pdf';
+import { useTheme } from '../../commom/ThemeContext';
 
 function Hero() {
+
+    const {theme, toggleTheme } = useTheme();
+
+    const themeIcon = theme === 'light' ? sun : moon;
+    const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+    const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+    const githubIcon = theme === 'light' ? githubLight : githubDark;
+
     return <section id="hero" className={styles.container}>
         <div className={styles.colorModeContainer}>
             <img className={styles.hero} src={heroImg} alt='Profile picture of Isuru Promuditha' />
-            <img className={styles.colorMode} src={themeIcon} alt='Color mode icon' />
+            <img className={styles.colorMode} src={themeIcon} onClick={toggleTheme} alt='Color mode icon' />
         </div>
         <div className={styles.info}>
             <h1>Isuru <br/> Promuditha</h1>
@@ -27,7 +40,7 @@ function Hero() {
                 <img src={linkedinIcon} alt='Linkedin icon'/>
                 </a>
             </span>
-            <p>With a passion for developing modern React web apps for commercial businesses.</p>
+            <p className={styles.description}>With a passion for developing modern React web apps for commercial businesses.</p>
             <a href={CV} download>
                 <button className='hover'>Resume</button>
             </a>
